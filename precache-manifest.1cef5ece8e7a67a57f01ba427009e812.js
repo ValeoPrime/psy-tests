eed99d9bab955b17b23826a5ba162bb680d0e606
Valentin <primarh111@mail.ru>
2020-11-09 09:49:27 +0300

    Updates

---
+++ precache-manifest.1cef5ece8e7a67a57f01ba427009e812.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592d6c1242ed3718c6f3a5327995c964",
+    "url": "/psy-tests/index.html"
+  },
+  {
+    "revision": "ea685c05a1c412e7095a",
+    "url": "/psy-tests/static/css/main.6ca6ab22.chunk.css"
+  },
+  {
+    "revision": "b64990846321c8029ce7",
+    "url": "/psy-tests/static/js/2.c7266c72.chunk.js"
+  },
+  {
+    "revision": "4f2dedb9f87ffe36ce515bb1189ae74f",
+    "url": "/psy-tests/static/js/2.c7266c72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea685c05a1c412e7095a",
+    "url": "/psy-tests/static/js/main.097775ce.chunk.js"
+  },
+  {
+    "revision": "926e4b69a1f93b884536",
+    "url": "/psy-tests/static/js/runtime-main.2fed1e71.js"
+  },
+  {
+    "revision": "ce673ed6e1e10b4f898a0e16b3a8ca1c",
+    "url": "/psy-tests/static/media/Mountains_Stars_Sky_Scenery_USA_Cascades_Mount_529129_2500x1686.ce673ed6.jpg"
+  },
+  {
+    "revision": "8e9a8607927fb12d65ba094ec6bdecea",
+    "url": "/psy-tests/static/media/biology-min.8e9a8607.jpg"
+  },
+  {
+    "revision": "cd9be381ca8ac8e56e8be3cb8a4e2754",
+    "url": "/psy-tests/static/media/chemistry-min.cd9be381.png"
+  },
+  {
+    "revision": "7fb6283aea33b6523bb4d70d332c2072",
+    "url": "/psy-tests/static/media/geography-min.7fb6283a.jpg"
+  },
+  {
+    "revision": "54ef2ae24d465e0ebf65430308cfd035",
+    "url": "/psy-tests/static/media/ozer-min.54ef2ae2.jpeg"
+  },
+  {
+    "revision": "38780a1bb15f18ae9614ef3fdb1a0455",
+    "url": "/psy-tests/static/media/pejzazhy_prirody_02.38780a1b.jpg"
+  },
+  {
+    "revision": "7609a723f92b48091bfbea220e107205",
+    "url": "/psy-tests/static/media/s1200.7609a723.jpg"
+  }
+]);
